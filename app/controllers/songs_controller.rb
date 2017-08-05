9f7037c022bd2b0b2923d8e3e2ca93de411656f5
Sara Hopper <sara.hopper@gmail.com>
2017-08-05 03:07:41 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    f params[:artist_id]
+    if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
